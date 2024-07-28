a2876dee3ab7c4a3d40d4fe35b19001dc98e953b
angelxqsi <2202007@utrivieramaya.edu.mx>
2024-07-27 21:41:32 -0500

    first commit

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAaZ-Udfcj4BvtNIdwxnKBma3TCSnNDwfE",
+  authDomain: "angell-76b86.firebaseapp.com",
+  projectId: "angell-76b86",
+  storageBucket: "angell-76b86.appspot.com",
+  messagingSenderId: "133797925800",
+  appId: "1:133797925800:web:0386b6707c48888d7cc39d"
+};
+
+// Initialize Firebase
+const app = initializeApp(FirebaseConfig);
+const bd = getFirestore(app);
+
+export default {
+  app,bd
+}
